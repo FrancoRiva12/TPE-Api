6b6ddc32c1a340de7abeef4c1477cdd180123301
FrancoRiva12 <francoriva72@gmail.com>
2023-11-13 19:11:32 -0300

    Actualizo router

--- controller/productoController.php
+++ controller/productoController.php
@@ -1,7 +1,7 @@
 <?php
 
+require '../config.php';
 require '../models/producto.php';
-require './config.php';
 
 class ProductoController {
     private $producto;
--- router.php
+++ router.php
@@ -2,61 +2,40 @@
 
 require 'config.php';
 
-// Lee la acción de la URL
-$action = 'home'; // Acción por defecto
-
-if (!empty($_GET['action'])) {
-    $action = $_GET['action'];
-}
+// lee accion de la URL
+$action = isset($_GET['action']) ? $_GET['action'] : 'home'; // Acción por defecto
 
 // Divide la acción en partes
 $params = explode('/', $action);
 
-// Determina qué página o controlador cargar según la acción
-$viewFolder = 'view/';
+// Determina qué controlador carga
 $controllerFolder = 'controller/';
 
-switch ($params[0]) {
-    case 'home':
-        include 'index.php'; // Carga la página principal
-        break;
-    case 'detalle':
-        include $viewFolder . 'detalle.php'; // Carga la página de detalle de ítem
-        break;
-    case 'items_por_categoria':
-        include $viewFolder . 'items_por_categoria.php'; // Carga la página de ítems por categoría
-        break;
-    case 'login':
-        include $viewFolder . 'login.php'; // Carga la página de inicio de sesión
-        break;
-    case 'mostrar':
-        include $viewFolder . 'mostrar.php'; // Carga la página de muestra de placas de video
-        break;
-    case 'crear':
-        include $controllerFolder . 'crear.php'; // Carga el controlador para crear ítems
-        break;
-    case 'eliminar':
-        include $controllerFolder . 'eliminar.php'; // Carga el controlador para eliminar ítems
-        break;
-    case 'modificar':
-        include $controllerFolder . 'modificar.php'; // Carga el controlador para modificar ítems
-        break;
-    case 'crear_categoria':
-        include $controllerFolder . 'crear_categoria.php'; // Carga el controlador para crear categorías
-        break;
-    case 'eliminar_categoria':
-        include $controllerFolder . 'eliminar_categoria.php'; // Carga el controlador para eliminar categorías
-        break;
-    case 'modificar_categoria':
-        include $controllerFolder . 'modificar_categoria.php'; // Carga el controlador para modificar categorías
-        break;
-    default:
-        echo '404 Page not found';
-        break;
+// obtiene el nombre del controlador
+$controllerName = $params[0] ?? 'home';
+
+// Obtiene nombre del metodo
+$methodName = $params[1] ?? 'index';
+
+// Incluye el controlador correspondiente
+$controllerFile = $controllerFolder . $controllerName . 'Controller.php';
+
+if (file_exists($controllerFile)) {
+    require $controllerFile;
+
+    // Instancia el controlador y llama al método
+    $controllerClassName = ucfirst($controllerName) . 'Controller';
+    $controller = new $controllerClassName($pdo);
+
+    // 
+    if (method_exists($controller, $methodName)) {
+        $controller->$methodName();
+    } else {
+        http_response_code(404);
+        echo json_encode(['error' => 'Método no encontrado']);
+    }
+} else {
+    http_response_code(404);
+    echo json_encode(['error' => 'Controlador no encontrado']);
 }
 ?>
-
-
-
-
-
